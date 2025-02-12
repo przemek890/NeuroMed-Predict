7452204c7ed2d2c4aa48c8509e6e06d17b2d52a5
przemek890 <przemek.899@wp.pl>
2025-02-12 22:32:37 +0100

    -

--- azure/deploy.sh
+++ azure/deploy.sh
@@ -9,6 +9,8 @@ if [ ! -f terraform.tfvars ]; then
   echo "Error: terraform.tfvars file not found. Exiting."
   exit 1
 fi
+
+# TODO - change to main branch
 git clone -b develop https://github.com/przemek890/NeuroMed-Predict.git
 mv terraform.tfvars NeuroMed-Predict/azure/terraform/
 cd NeuroMed-Predict/azure
